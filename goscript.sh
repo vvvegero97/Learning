724aa4341a8aee7b75ee71a52ce3d0bf9233bc26
marsikmarsik123 <marsikmarsik123@rambler.ru>
2021-11-14 16:55:13 +0300

    Extra Task vers2

--- goscript.sh
+++ goscript.sh
@@ -1,28 +1,72 @@
 #!/bin/bash
 
+#Start asking cycle
+
+tempcnt=0
+
+while [ $tempcnt -eq 0 ]
+do
+
+
 #ask user
 
-echo "Yes or No?"
-read ANSWR
+	echo 'Create hello go file? "Yes" or "No"?'
+	read ANSWR
 
 #check answer
 
-if [ "$ANSWR" == "No" ]; then
-	echo "BYE!"
-elif [ "$ANSWR" == "Yes" ]; then
-	mkdir /home/VVEGERO/otp/go/go/src/hello
-	cd /home/VVEGERO/otp/go/go/src/hello
-	touch hello.go
-	file=/home/VVEGERO/otp/go/go/src/hello/hello.go
-	echo "package main" > $file
-	echo "" >> $file
-	echo 'import "fmt"'>> $file
-	echo "" >> $file
-	echo "func main() {" >> $file
-	echo 'fmt.Printf("Hello, World!\n")' >> $file
-	echo "}" >> $file
-	go build $file
-	./hello
-else 
-	echo "WRONG ANSWER!!!!"
-fi
+	if [ "$ANSWR" == "No" ]; then
+
+##Negative answer -> Say goodbye and finish cycle
+
+		echo "BYE!"
+		tempcnt=$((tempcnt + 1))
+		break
+
+	elif [ "$ANSWR" == "Yes" ]; then
+
+##Positive answer -> Create go script and finish cycle 
+
+###Create hello directory in src, if not created already
+
+		if [ -d /home/VVEGERO/otp/go/go/src/hello ]; then
+			echo "/src/hello directory already exists."
+		else 
+			mkdir /home/VVEGERO/otp/go/go/src/hello
+		fi
+		cd /home/VVEGERO/otp/go/go/src/hello
+
+
+###Create hello.go file in /src/hello, if not created already
+
+		if [ ! -f ./hello.go ]; then
+		touch hello.go
+		fi
+
+###Add go script in hello.go
+
+		file=/home/VVEGERO/otp/go/go/src/hello/hello.go
+		echo "package main" > $file
+		echo "" >> $file
+		echo 'import "fmt"'>> $file
+		echo "" >> $file
+		echo "func main() {" >> $file
+		echo 'fmt.Printf("Hello, World!\n")' >> $file
+		echo "}" >> $file
+
+###Create exec file hello and launch it
+
+		if [ -f ./hello ]; then
+			rm hello
+		fi
+		go build $file
+		./hello
+		tmpcnt=$((tmpcnt + 1))
+		break
+	else 
+
+##Unknown answer -> Ask again
+
+		echo "WRONG ANSWER!!!!"
+	fi
+done
